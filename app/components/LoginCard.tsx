34bf0938e5a863f931b1df87743401571bd0e4fd
Joe Ghaida <josephghaida@gmail.com>
2023-12-28 20:02:03 -0500

    cleaned login

--- app/components/LoginCard.tsx
+++ app/components/LoginCard.tsx
@@ -16,49 +16,14 @@
 
 import React, { useEffect } from 'react';
 
-const LoginCard = ({ client_id, client_secret }) => {
+const LoginCard = ({ client_id }) => {
 
   const handleLogin = () => {
     window.location.replace(`${'https://accounts.spotify.com/authorize'}?response_type=code&client_id=${client_id}&redirect_uri=${'http://localhost:3000'}`);
   };
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const authorizationCode = urlParams.get('code');
-
-    if (authorizationCode) {
-      // Make a POST request using the fetch API
-      fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        form: {
-          code: authorizationCode,
-          redirect_uri: 'http://localhost:3000',
-          grant_type: 'authorization_code'
-        },
-        headers: {
-          'Authorization': `Basic ${btoa(`${client_id}:${client_secret}`)}`,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        json:true
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        const accessToken = data.access_token;
-        // Do something with the access token, like storing it in state or localStorage
-        console.log('Access Token:', accessToken);
-      })
-      .catch(error => {
-        console.error('Error exchanging authorization code for access token:', error);
-      });
-    }
-  }, []);
-
-  return (
-    <div>
-      <button onClick={handleLogin}>Login Spotify</button>
-    </div>
-  );
+  
+  return <button onClick={handleLogin}>Login Spotify</button>;
 };
 
 export default LoginCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,39 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
-import ProductCard from './components/LoginCard'
-import querystring from 'querystring';
-import crypto from 'crypto';
+import { useSearchParams } from 'next/navigation'
 import LoginCard from './components/LoginCard';
 
 
 
-export default function Home() {
+export default function Home({ searchParams } : { searchParams: { [key: string]: string | string[] | undefined }}) {
 
-  console.log("this is server code");
+  if( "code" in searchParams ) {
 
-   
+    const { code: authorizationCode } = searchParams;
+    const [ client_id, client_secret ] = [ process.env.CLIENT_ID, process.env.CLIENT_SECRET ];
+    console.log("-------------");
+    
 
-  return (
-    <>
-    <LoginCard client_id={process.env.CLIENT_ID} client_secret={process.env.CLIENT_ID}> </LoginCard>
-    </>
+
+
+
+    fetch( 'https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        //@ts-ignore
+        body: new URLSearchParams({ code: authorizationCode, redirect_uri: 'http://localhost:3000', grant_type: 'authorization_code' }),
+        headers: { 'Authorization': `Basic ${ btoa(`${ client_id }:${ client_secret }`) }`, 'Content-Type': 'application/x-www-form-urlencoded' }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        const accessToken = data.access_token;
+        // Do something with the access token, like storing it in state or localStorage
+        console.log('Access Token:', accessToken);
+      })
+      .catch( error => console.error( 'Error exchanging authorization code for access token:', error ));
+
+    return <div>We got the code: { authorizationCode }</div>
+
+  };
   
-  )
-}
+  return <LoginCard client_id={ process.env.CLIENT_ID }/>;
+
+};

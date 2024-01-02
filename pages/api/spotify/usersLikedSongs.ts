cc6a0ebac1683bc512315cad81972a4bf016ffcf
Joe Ghaida <josephghaida@gmail.com>
2024-01-01 20:43:40 -0500

    ok

--- pages/api/spotify/usersLikedSongs.ts
+++ pages/api/spotify/usersLikedSongs.ts
@@ -8,15 +8,16 @@ interface Req extends NextApiRequest {
 
 const handler = async ( req : NextApiRequest, res: NextApiResponse ) => {    
     const { token } = req.query;
-    const response = await fetch("https://api.spotify.com/v1/me/tracks", { headers: { Authorization: 'Bearer ' + token }})
-    .then( r => { if( r.status === 200 ) return r; else throw r })
+    await fetch("https://api.spotify.com/v1/me/tracks", { headers: { Authorization: 'Bearer ' + token }})
+    .then( r => { if( r.status === 200 ) return r; else { console.log( res.status, "<<<<" ); console.log( res);
+     throw r }})
     .then( r => r.json( ))
     .then( async songsReq => res.status(200).json( songsReq ))
     .catch( error => {
         console.log("BAD");
         console.log( typeof error );
-        
-        res.status(500).json( error );
+        // console.log( error );
+        res.status(500).json({ message: error });
         // console.log( "ERR", error );
     })
 };

0f0410367ee47c371bfdf6d509e612c97e51d561
Joe Ghaida <josephghaida@gmail.com>
2024-01-01 19:57:55 -0500

    clean up

--- app/components/MainFrame.tsx
+++ app/components/MainFrame.tsx
@@ -6,35 +6,9 @@ const MainFrame = ({ token } : { token: string; }) => {
     const [ listOfSongs, setSongList ] = useState([]);
     
     const getSomeLikedSongs = async () => {
-
       const userLikedSongs = await fetch(`/api/spotify/usersLikedSongs/?token=${ token }`).then( res => res.json( ));
       console.log( userLikedSongs );
       setSongList( userLikedSongs.items );
-
-
-        // console.log("using this token", token );
-        // const response = await fetch("https://api.spotify.com/v1/me/tracks", {
-        //     headers: {
-        //         Authorization: 'Bearer ' + token
-        //       }
-        // })
-        // .then( async res => {
-        //   console.log( res );
-        //   if ( res.ok ){
-        //       console.log(res)
-        //       //@ts-ignore
-        //       setSongList( res.items );
-        //   }
-        //   else {
-        //     console.log("bad");
-            
-        //       throw res;
-        //     }
-          
-        // })
-        // .catch( error => console.log( "ERR", error ))
-        // // console.log(response, "<<")
-
     };
 
     useEffect(( ) => { 
